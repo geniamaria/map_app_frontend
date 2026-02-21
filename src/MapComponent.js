44601e069e5365448cb1da256d9a573495e04ad7
Maria Genia Abilio Moiane <mariageniaabilio@gmail.com>
2026-02-21 16:44:51 +0200

    Refactor MapComponent to use custom icons and improve user marker handling

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,34 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import io from "socket.io-client";
+import L from "leaflet";
 
 const socket = io("https://map-app-realtime.onrender.com");
 
+const myIcon = new L.Icon({
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
+
+const otherIcon = new L.Icon({
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-red.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
+
 function MapComponent() {
-  const [position, setPosition] = useState(null);
-  const [users, setUsers] = useState({});
+  const [position, setPosition] = useState(null); 
+  const [users, setUsers] = useState({}); 
 
   useEffect(() => {
-    // Pegar localização do utilizador
     navigator.geolocation.watchPosition((pos) => {
       const coords = {
         lat: pos.coords.latitude,
-        lng: pos.coords.longitude
+        lng: pos.coords.longitude,
       };
       setPosition(coords);
-      socket.emit("send-location", coords);
+      socket.emit("send-location", coords); 
     });
 
-    // Receber utilizadores conectados
+   
     socket.on("users-update", (data) => {
       setUsers(data);
     });
 
+   
+    return () => {
+      socket.off("users-update");
+    };
   }, []);
 
   return (
     <MapContainer
-      center={position || [-25.9653, 32.5892]}
+      center={position || [-25.9653, 32.5892]} 
       zoom={13}
       style={{ height: "100vh", width: "100%" }}
     >
@@ -37,8 +59,13 @@ function MapComponent() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {Object.values(users).map((user, index) => (
-        <Marker key={index} position={[user.lat, user.lng]}>
+     
+      {Object.entries(users).map(([id, user]) => (
+        <Marker
+          key={id}
+          position={[user.lat, user.lng]}
+          icon={id === socket.id ? myIcon : otherIcon} 
+        >
           <Popup>
             Latitude: {user.lat} <br />
             Longitude: {user.lng}
